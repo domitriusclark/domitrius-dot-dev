e3aa90364afd9b828e5160a657d8f9f456ccbbeb
Domitrius Clark <domitriusaclark@gmail.com>
2022-12-16 18:57:21 -0500

    Removed errored trailing comma in config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://domitrius.dev",,
+  site: "https://domitrius.dev",
   integrations: [astroImageTools, tailwind()],
 });
